397b8f9c8bcd3b7ebf11c404f459dbfbb27ae9a3
Dremorian <dremorian@yandex.ru>
2017-08-11 14:24:36 +0300

    Изменен внешний вид списка домов в клиенте

--- frontend/views/site/el.php
+++ frontend/views/site/el.php
@@ -6,7 +6,7 @@
     <link rel=stylesheet href=https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel=stylesheet>
     <link href=/static/sweetalert/dist/sweetalert.css rel=stylesheet>
-    <link href=/static/css/app.38844ba30919c2aabf87d5ce8cc98e52.css rel=stylesheet>
+    <link href=/static/css/app.5c86ca4670da28b509d2d367ab4897b2.css rel=stylesheet>
 </head>
 <body>
 <div class=container>
@@ -20,9 +20,9 @@
 <script src=https://code.jquery.com/jquery-3.2.1.min.js></script>
 <script src=https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js></script>
 <script src=/static/sweetalert/dist/sweetalert-dev.js></script>
-<script type=text/javascript src=/static/js/manifest.2084d3255e1e9b22e04c.js></script>
-<script type=text/javascript src=/static/js/vendor.f1eaf850adb53035032d.js></script>
-<script type=text/javascript src=/static/js/app.63a295c0dece1a0b16b0.js></script>
+<script type=text/javascript src=/static/js/manifest.4965b9113ccbba60899b.js></script>
+<script type=text/javascript src=/static/js/vendor.af57a394cfa75dcc8fc4.js></script>
+<script type=text/javascript src=/static/js/app.f8ce5232a58e298418f6.js></script>
 
 </body>
 </html>
